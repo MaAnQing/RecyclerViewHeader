7fb892f553c1d6f12b8b81a03e350c8bfdbabce7
madroid <madroidmaq@gmail.com>
2015-12-15 14:13:42 +0800

    init project

---
+++ library/src/main/java/com/github/madroid/library/RecyclerViewHeader.java
@@ -0,0 +1,8 @@
+package com.github.madroid.library;
+
+/**
+ * created by madroid at 2015-12-15
+ */
+public class RecyclerViewHeader {
+    private static final String TAG = "RecyclerViewHeader";
+}
